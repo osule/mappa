9450148c1f44fd574f16a76dcbd2dd87f7819fe5
Oluwafemi Sule <oluwafemisule@outlook.com>
2017-11-10 19:58:02 +0100

    add sockets, redis pass db and publisher to routes

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const redis = require('redis');
 const configureRoutes = require('./routes');
+const db = require('./models');
+const SocketServer = require('ws').Server;
 
-configureRoutes(app);
+const app = express();
+const sub = redis.createClient(), pub = redis.createClient();
+const wss = new SocketServer({ server: app });
+
+
+app.use(bodyParser.json());
+
+configureRoutes(app, db, {pub}); // publish to Redis.
+
+// subscribe to Redis channel updates and broadcast to websockets.
 
 app.listen(5000, () => console.log('Example app listening on port 5000!'));
